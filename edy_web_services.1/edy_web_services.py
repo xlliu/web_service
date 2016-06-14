a98ed5d2527f504c6c13387dcc81d6c94484a435
xlliu <liu.xuelong@163.com>
2016-06-14 04:19:59 +0000

    s

--- edy_web_services.1/edy_web_services.py
+++ edy_web_services.1/edy_web_services.py
@@ -93,7 +93,7 @@ def show_excel_info(version, pid, skip, limit):
     document_project = getattr(g.mongo_collection, _pid)
     dpt_1 = document_project.find({"0d版本": version},{"_id": 0,"0d开始时间":0,"0d结束时间":0,"0d序号":0, "0d用户":0}).skip(skip).limit(limit)
     # data_list = [f_dpt_1 for f_dpt_1 in dpt_1 if "k_list" not in f_dpt_1]
-    # data_list_1 = []
+    data_list_1 = []
     for dv in dpt_1:
         logger.info(str(dv))
         if "k_list" in dv:
