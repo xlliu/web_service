262b8943e3e7c83ed8ab19499858209a6275bfa2
xlliu <liu.xuelong@163.com>
2016-06-14 03:33:34 +0000

    s

--- web_services_kill.sh
+++ web_services_kill.sh
@@ -1,4 +1,4 @@
 kill -9 `cat edy_web_services.pid`
 echo "pid: `cat edy_web_services.pid` KO Success"
 kill -9 `cat edy_web_services_1.pid`
-echo "pid: `cat edy_web_services.pid` KO Success"
+echo "pid: `cat edy_web_services_1.pid` KO Success"
